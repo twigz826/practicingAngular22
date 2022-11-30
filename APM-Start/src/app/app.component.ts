d0bf34b189f259964580b524f77fabaa6bbde3ec
Thomas Twigden <ttwigden@deloitte.co.uk>
2022-11-30 18:49:07 +0000

    First commit - setting up app component, module and product list

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:
+    `
+    <pm-products></pm-products>
+    `
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+
 }
